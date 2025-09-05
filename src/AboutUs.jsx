fc29ffe38b020e24b05d735643ffb45a6e2c999a
K-GIT23 <ksql2010@gmail.com>
2025-09-05 16:32:42 -0400

    update app.css and aboutus

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -5,19 +5,31 @@ function AboutUs() {
   return (
     <div className="about-us-container">
       {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
+      <p className="about-us-description">Welcome to Pleasant Plantings, where plants bring serenity!</p>
       <p className="about-us-content">
-        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
-        high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
-        more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
-        plant enthusiast.
-      </p>
-      {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
-      <p className="about-us-content">
-        Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. 
-        Whether you're a seasoned gardener or just starting your green journey, we're here to support you every step of 
-        the way. Feel free to explore our collection, ask questions, and let us help you find the perfect plant for your 
-        home or office.
+        At Pleasant Plantings, we are passionate about bringing nature closer to you. Who knew
+        that natural elements like plants can promote calmness, contentment, overall well-being, and
+        enchanced focus - Oh we did! That is why we have a variety of plants available for your increased enjoyment of life. 
+        </p>
+        <p className="about-us-content">
+            Humans have a strong natural connection to nature and living plant life. Just having plants
+            around us offers psychological and even physical benefits. Plants create a calm, 'healing'
+            space of peace and serenity for reducing stress and anxieties of life. We naturally become
+            calmer, more content, relaxed.
+         </p>        
+        {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
+        <p className="about-us-content">
+        Whether you choose the beauty of a floral aromatic selection, plants you can care for, 
+        edibles, or other types we have available, plants can bring new life to your day and your 
+        outlook. 
+        </p>
+        <p className="about-us-content">
+        Feel free to explore our collections, ask questions, and let us help you find the
+        perfect plant for your home or office. Our team of experts is dedicated to ensuring 
+        that each plant meets our strict standards of quality and care.
+        </p> 
+        <p className="about-us-content">
+            Pleasant Shopping!
       </p>
       {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
 
