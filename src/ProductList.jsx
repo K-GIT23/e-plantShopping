e40750a0360a27057501c930bf0d2921bf72c81f
K-GIT23 <ksql2010@gmail.com>
2025-09-10 16:53:06 -0400

    correcting productlist code

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,17 +2,18 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { useDispatch } from 'react-redux';
-import { addItem } from './CartSlice';
+//import { addItem } from './CartSlice';
  
 
-const dispatch = useDispatch();
+
 
 
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
-
+    const dispatch = useDispatch();
+    
     const handleAddToCart = (product) => {
         dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
       
