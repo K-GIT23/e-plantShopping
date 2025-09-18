d8dad25f140e768babefaa774a21c2085854ca48
K-GIT23 <ksql2010@gmail.com>
2025-09-18 18:55:41 -0400

    add task2

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,20 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+       state.items.push(action.payload);
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
+        console.log('Remove item reducer fired!', action.payload); 
     },
     updateQuantity: (state, action) => {
-
-    
+        const { name, quantity } = action.payload; // Destructure the product name and new quantity from the action payload
+        // Find the item in the cart that matches the given name
+        const itemToUpdate = state.items.find(item => item.name === name);
+        if (itemToUpdate) {
+            console.log('Update Quantity reducer fired!', action.payload); 
+           itemToUpdate.quantity = quantity; // If the item is found, update its quantity to the new value
+        }    
     },
   },
 });
